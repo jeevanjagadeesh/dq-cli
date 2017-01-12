1cd14b50af1c03c6d65f29d55a4f7e5b9fbc8ec4
Jeevan Jagadeesh <jeevan-kumar.jagadeesh@hpe.com>
2017-01-12 17:54:36 +0530

    use log file to create xml

--- dqCliLogReader.js
+++ dqCliLogReader.js
@@ -4,6 +4,7 @@ var path = require("path");
 var fs = require('fs');
 var _ = require('lodash');
 var pug = require('pug');
+var async = require('async');
 var logPatternConfig = require('./config.js').logpattern;
 
 var log = logger.createLogger({
@@ -17,7 +18,9 @@ var config = require('./properties/config.json');
 var baseDir = __dirname;
 
 var mixinStr = fs.readFileSync(baseDir + '/mixins/QATrackerReport.pug', 'utf8')
-var xmlTemplate = pug.compile(mixinStr, { pretty: true });
+var xmlTemplate = pug.compile(mixinStr, {
+  pretty: true
+});
 
 var DEFAULT_PARAMS = {
   src: "oie"
@@ -44,61 +47,86 @@ function parseParams(consoleParams) {
 
 // load console parameters
 var cParams = parseParams(process.argv.slice(2));
-log.info({ params: cParams}, 'Using following params');
+log.info({ params: cParams }, 'Using following params');
 
 main();
 
 function main() {
   log.debug(' ******** Starting Log Reading ******** ');
   var jsonData = _.cloneDeep(config);
-  // just testing
-  var testCases = [
-	          {
-	            'Name': 'oie-oieexportobjects_1-case1',
-	            'description': 'test  description',
-	            'status': 'Pass'
-	          },
-	          {
-			  	 'Name': 'oie-oieexportobjects_1-case2',
-			  	 'description': 'test  description 2',
-			  	 'status': 'Failed'
-	          },
-              {
-				 'Name': 'oie-oieexportobjects_1-case3',
-				 'description': 'test  description 3',
-				 'status': 'Failed'
-	          }
-      ];
-  jsonData.testCases = testCases;
-  var countBy = _.countBy(jsonData.testCases, 'status');
-  jsonData.passedTestCaseCount = countBy.Pass;
-  jsonData.failedTestCaseCount = countBy.Failed;
-  jsonData.totalTestCaseCount = jsonData.testCases.length;
-
-  var trackerXml = xmlTemplate(jsonData);
-  //log.info(trackerXml);
-console.log(trackerXml);
-  //log.info(jsonData);
- //log.info({ logPatternConfig: logPatternConfig.success }, 'logPatternConfig');
+  var testCases = [];
   var plugin = cParams.src;
-  var xlspath = path.join(baseDir, 'input', plugin.concat('.xls'));
-  var logName = path.join(baseDir, 'logs', 'oie-oieimportobjects-case1.log');
-  searchLog(logName, function(err, success) {
-    //console.log(success);
+  jsonData.pluginName = plugin;
+  var xlspath = path.join(baseDir, 'input', plugin.concat('.xlsx'));
+  var workbook = excel2Json.readExcelFile(xlspath);
+  excel2Json.to_json(workbook, function(result) {
+    // iterate each worksheet
+    async.forEachOf(result, function(value, key, callback1) {
+      async.forEach(value, function(elementOfArray, callback2) {
+        if (elementOfArray.ExecuteOption !== 'Skip') {
+          var logName = path.join(baseDir, 'logs', elementOfArray.TestCaseID + '.log');
+          var testCase = {};
+          searchLog(logName, function(err, status, failureMsg) {
+            if (!err) {
+              testCase.Name = elementOfArray.TestCaseID;
+              testCase.status = status;
+              testCase.exceptionMessage = failureMsg;
+              testCases.push(testCase);
+            }
+            callback2(err);
+          });
+        } else callback2();
+
+      }, function(err) {
+        if (err) {
+          log.error({err: err}, 'Error from async worksheet row looping');
+        }
+        callback1();
+        log.info("processing all elements completed");
+
+      });
+    }, function(err) {
+      if (err) {
+        log.error({err: err}, 'Error xls worksheet looping');
+      }
+      jsonData.testCases = testCases;
+      var countBy = _.countBy(testCases, 'status');
+      jsonData.passedTestCaseCount = countBy.Passed;
+      jsonData.failedTestCaseCount = countBy.Failed;
+      jsonData.totalTestCaseCount = jsonData.testCases.length;
+      var trackerXml = xmlTemplate(jsonData);
+      console.log(trackerXml);
+      var xmlpath = path.join(baseDir, 'uploadToReport', "QATrackerReport-" + plugin + '.xml');
+      fs.writeFile(xmlpath, trackerXml, function (err) {
+	    if (err)
+	      return console.log(err);
+	      console.log('File written succesfully!!!');
+      });
+    });
   });
 }
 
 function searchLog(filename, cb) {
   //var success, failure;
   // grep is better???
+  //console.log(filename);
   fs.readFile(filename, function(err, data) {
     if (err) {
       return cb(err);
     }
     if (data.indexOf(logPatternConfig.success) > -1) {
-      return cb(err, 'success');
+      return cb(err, 'Passed');
+    } else {
+      var failMsg;
+      _.forEach(logPatternConfig.failure, function(failureMsg) {
+        if (data.indexOf(failureMsg) > -1) {
+          failMsg = failureMsg;
+          // break the loop, if error found
+          return false;
+        }
+      });
+      return cb(err, 'Failed', failMsg);
     }
-    // TODO error logs
   });
 }
 
