fc9bc694ef498e5b2ca7ce16b92d48554af54903
Jeevan Jagadeesh <jeevan-kumar.jagadeesh@hpe.com>
2017-01-17 19:44:07 +0530

    remove unused java code

--- QATrackImport/importXML1.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-SET CLASSPATH=.\QATrackImport\QATrackImport.jar;.\QATrackImport\lib\apache-mime4j-0.6.jar;.\QATrackImport\lib\commons-codec-1.3.jar;.\QATrackImport\lib\commons-logging-1.1.1.jar;.\QATrackImport\lib\httpclient-4.0.1.jar;.\QATrackImport\lib\httpcore-4.0.1.jar;.\QATrackImport\lib\httpmime-4.0.1.jar
-"%JAVA_HOME%\bin\java" com.informatica.qatrack.ImportXML %*
--- lib/QATrackImport/importXML.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-SET CLASSPATH=.\QATrackImport.jar;.\lib\apache-mime4j-0.6.jar;.\lib\commons-codec-1.3.jar;.\lib\commons-logging-1.1.1.jar;.\lib\httpclient-4.0.1.jar;.\lib\httpcore-4.0.1.jar;.\lib\httpmime-4.0.1.jar
-"%JAVA_HOME%\bin\java" com.informatica.qatrack.ImportXML %*
