3df77151ab17a8401c5b924b11abd23a57ea4d1f
Jeevan Jagadeesh <jeevan-kumar.jagadeesh@hpe.com>
2017-01-17 12:50:46 +0530

    invoke log reader after batch execution

--- dq_cli.js
+++ dq_cli.js
@@ -16,6 +16,8 @@ log.level('debug');
 var excel = require('./lib/excel2Json.js');
 var config = require('./properties/config.json');
 var excel2Json = new excel(log);
+var dqLog = require('./dqCliLogReader.js');
+var dqCliLogReader = new dqLog(log);
 var baseDir = __dirname;
 
 var DEFAULT_PARAMS = {
@@ -43,7 +45,7 @@ function parseParams(consoleParams) {
 
 // load console parameters
 var cParams = parseParams(process.argv.slice(2));
-log.info({params: cParams}, 'Using following params');
+log.info({ params: cParams }, 'Using following params');
 
 main();
 
@@ -51,22 +53,21 @@ function main() {
   log.debug(' ******** Starting ******** ');
   var timeid = moment().format('YYYYMMDD[_]hmm');
   log.info(' Current timestamp: ' + timeid);
-  log.info(' OS Type: ' + os.type() +' & OS Platform: ' + os.platform() + ' & OS arch: ' + os.arch());
+  log.info(' OS Type: ' + os.type() + ' & OS Platform: ' + os.platform() + ' & OS arch: ' + os.arch());
   var plugin = cParams.src;
-  var xlspath = path.join(baseDir, 'input', plugin.concat('.xls'));
+  var xlspath = path.join(baseDir, 'input', plugin.concat('.xlsx'));
   var uploadToQaTrackExec = path.join(baseDir, 'QATrackImport', 'importxml.bat');
   var qatrackimportxml = path.join(baseDir, 'uploadToReport', "QATrackerReport-" + plugin + '.xml');
-  var qatrackurl = 'http://psrlxpamqa1:8080/qatrack/servlet/ImportXMLServlet'  
-  
+  var qatrackurl = 'http://psrlxpamqa1:8080/qatrack/servlet/ImportXMLServlet'
+
   //log.info(' Windows? : ' + os.platform().includes('win'));
-  if (os.platform().toLowerCase().includes('win')){
-  var infacmd_op_file = path.join(baseDir, 'output', plugin.concat('.bat'));
-  var infacmd_log_file = path.join(baseDir, 'logs', plugin.concat('.log'));
-  var infacmd_type = 'infacmd.bat';
-  }
-  else{
-  var infacmd_op_file = path.join(baseDir, 'output', plugin.concat('.sh'));
-  var infacmd_type = './infacmd.sh';
+  if (os.platform().toLowerCase().includes('win')) {
+    var infacmd_op_file = path.join(baseDir, 'output', plugin.concat('.bat'));
+    var infacmd_log_file = path.join(baseDir, 'logs', plugin.concat('.log'));
+    var infacmd_type = 'infacmd.bat';
+  } else {
+    var infacmd_op_file = path.join(baseDir, 'output', plugin.concat('.sh'));
+    var infacmd_type = './infacmd.sh';
   }
   //Take backup of output file if already exists
   var backup_infacmd_op_file = path.join(baseDir, 'output', plugin.concat('_').concat(timeid).concat('.bat'));
@@ -75,23 +76,25 @@ function main() {
   log.info(' isFileExist: ' + isFileExist);
   var isLogFileExist = fs.existsSync(infacmd_log_file);
   log.info(' isLogFileExist: ' + isLogFileExist);
-  
+
   if (isFileExist) {
     mv(infacmd_op_file, backup_infacmd_op_file, function(err) {});
   }
 
-    if (isLogFileExist) {
+  if (isLogFileExist) {
     mv(infacmd_log_file, backup_infacmd_log_file, function(err) {});
   }
-  
+
   var workbook = excel2Json.readExcelFile(xlspath);
+  var xlsData;
   excel2Json.to_json(workbook, function(result) {
+    xlsData = result;
     //log.info({result: result}, 'result of excel read');
     async.forEachOf(result, function(value, key, callback) {
       var fileName = key + '.txt';
       fs.readFile(path.join(baseDir, 'templates', fileName), 'utf8', function(err, template) {
         if (err) {
-          log.info({fileName: fileName}, 'No file');
+          log.info({ fileName: fileName }, 'No file');
           return callback('No file -- ' + fileName);
         }
         callback(err, template);
@@ -106,20 +109,18 @@ function main() {
             // replace config values
             _.forEach(config, function(value, key) {
               data = stringReplace(data, '<<' + key + '>>', value);
-            });            
+            });
 
-			// replace infacmd_type attributes in templates
-              data = stringReplace(data, '<<infacmd_type>>', infacmd_type);
-			  data = stringReplace(data, '<<LogFileName>>', infacmd_log_file);
-			
-			//log.info({data: data}, 'command output');
-			
+            // replace infacmd_type attributes in templates
+            data = stringReplace(data, '<<infacmd_type>>', infacmd_type);
+            data = stringReplace(data, '<<LogFileName>>', infacmd_log_file);
+            //log.info({data: data}, 'command output');
             writeToExecutable(infacmd_op_file, data);
           }
           callback();
         }, function(err) {
           if (err) {
-            log.error({err: err}, 'Error from async array looping');
+            log.error({ err: err }, 'Error from async array looping');
           }
           // for every worksheet complete, add line break
           writeToExecutable(infacmd_op_file, "\n");
@@ -128,32 +129,42 @@ function main() {
       });
     }, function(err) {
       if (err) {
-        log.error({err: err}, 'Error JSON array looping');
+        log.error({ err: err }, 'Error JSON array looping');
+        // Uncomment the below line
+        //process.exit(1);
       }
       // File creation complete, invoke the script
-	  log.info(infacmd_op_file,' :infacmd_op_file');
-	  
+      log.info(infacmd_op_file, ' :infacmd_op_file');
+
       childProcess.exec(infacmd_op_file, function(err, stdout, stderr) {
         if (err) {
-          log.error({err: err}, 'Error in executing file');
+          log.error({ err: err }, 'Error in executing file');
+          // Uncomment the below line
+          //process.exit(1);
         }
-        log.info({stdout: stdout, stderr: stderr }, ' Execution done');
-        log.info("processing JSON Array done");
-		
-		log.info(uploadToQaTrackExec,' :uploadToQaTrackExec');
-		log.info(qatrackurl,' :qatrackurl');
-		log.info(qatrackimportxml,' :qatrackimportxml');
-		
-			  childProcess.exec(uploadToQaTrackExec +' '+ qatrackurl +' '+ qatrackimportxml, function(err, stdout, stderr) {
-        if (err) {
-          log.error({err: err}, 'Error in executing file');
-        }
-        log.info({stdout: stdout, stderr: stderr }, ' Execution of QA Track Upload done');
-        log.info("processing JSON Array done");
+        log.info({ stdout: stdout, stderr: stderr }, ' Execution done');
+        dqCliLogReader.generateExecutionXml(xlsData, plugin, function(err, status) {
+          if (err) {
+            log.error({err: err }, 'Error in generating Xml');
+            // must exit
+            log.info('ERROR Exiting');
+            process.exit(1);
+          }
+          // XML generated invovke the java code with xml
+          log.info(uploadToQaTrackExec, ' :uploadToQaTrackExec');
+          log.info(qatrackurl, ' :qatrackurl');
+          log.info(qatrackimportxml, ' :qatrackimportxml');
+          childProcess.exec(uploadToQaTrackExec + ' ' + qatrackurl + ' ' + qatrackimportxml, function(err, stdout, stderr) {
+            if (err) {
+              log.error({ err: err }, 'Error in executing file');
+              process.exit(1);
+            }
+            log.info({ stdout: stdout, stderr: stderr }, ' Execution of QA Track Upload done');
+            log.info("Execution Complete!!! Exiting");
+            process.exit(0);
+          });
+        });
       });
-		
-      });
-
     });
   });
 }
@@ -172,6 +183,6 @@ function writeToExecutable(fileName, data) {
 }
 
 process.on('uncaughtException', function(err) {
-  log.error({err: err}, 'uncaughtException ');
+  log.error({ err: err }, 'uncaughtException ');
   process.exit(1);
 });
